d30677dbc48f494f61579dc18fe854f09af9cee2
Narek12345 <narekbayanduryan16@gmail.com>
2025-01-11 15:26:33 +0300

    Подключил tailwindcss, прописав в конфигурационных файлах.

--- frontend/src/components/CryptocurrencyCard.jsx
+++ frontend/src/components/CryptocurrencyCard.jsx
@@ -3,7 +3,17 @@ import {Card} from "antd";
 function CryptocurrencyCard() {
 	return (
 		<div>
-			<Card title="Default size card" extra={<a href="#">More</a>} style={{ width: 300 }}>
+			<Card 
+				title={
+					<div className="flex items-center">
+						<img src="https://s2.coinmarketcap.com/static/img/coins/64x64/1.png"/>
+						<span>Bitcoin</span>
+					</div>
+				}
+				extra={<a href="#">More</a>}
+				style={{
+					width: 300
+				}}>
     			<p>Card content</p>
     			<p>Card content</p>
     			<p>Card content</p>
